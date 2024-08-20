110ada9b11fd86f1fd548a7ef6a2bd703c82ecf7
Szymon Iwacz <szymon@iwacz.pl>
2024-08-20 22:19:24 +0200

    Update application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import HelloWorld from "../components/HelloWorld";
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <HelloWorld />,
-    document.getElementById("root")
-  );
+  const container = document.getElementById("root");
+  if (container) {
+    const root = createRoot(container); // createRoot(container!) if you use TypeScript
+    root.render(<HelloWorld />);
+  }
 });
